7b25b8b071fc0f6e1bc225913feec8067274f07f
nifx <you@example.com>
2013-05-12 14:58:37 +0200

    added bash script to download all necessary tools automatically

---
+++ get_tools.bash
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+##############################################################################
+# osmosis
+##############################################################################
+mkdir -p osmosis
+pushd osmosis
+rm -rf *
+wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
+tar xvfz osmosis-latest.tgz
+chmod a+x bin/osmosis
+popd
+
+##############################################################################
+# osmconvert
+##############################################################################
+mkdir -p osmconvert
+pushd osmconvert
+rm -rf *
+wget http://m.m.i24.cc/osmconvert32
+chmod a+x osmconvert32
+popd
+
+##############################################################################
+# osmfilter
+##############################################################################
+mkdir -p osmfilter
+pushd osmfilter
+rm -rf *
+wget http://m.m.i24.cc/osmfilter32
+chmod a+x osmfilter32
+popd
+
+##############################################################################
+# splitter
+##############################################################################
+FILENAME="splitter-r304"
+mkdir -p splitter
+pushd splitter
+rm -rf *
+wget http://www.mkgmap.org.uk/download/$FILENAME.tar.gz
+tar xvfz $FILENAME.tar.gz
+rm $FILENAME.tar.gz
+mv $FILENAME/* .
+rm -rf $FILENAME
+popd
+
+##############################################################################
+# mkgmap
+##############################################################################
+FILENAME="mkgmap-r2596"
+mkdir -p mkgmap
+pushd mkgmap
+rm -rf *
+wget http://www.mkgmap.org.uk/download/$FILENAME.tar.gz
+tar xvfz $FILENAME.tar.gz
+rm $FILENAME.tar.gz
+mv $FILENAME/* .
+rm -rf $FILENAME
+popd
